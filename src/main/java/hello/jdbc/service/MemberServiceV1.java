c0d73d8b86cf56cdc3310c8d79b8755c5b172564
shinyehwan <jghisjl007@naver.com>
2022-07-17 00:17:41 +0900

    트랜잭션 이해 적용2 까지 모두 마무리

---
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,33 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+/**
+ * 기본 동작, 트랜잭션이 없어서 문제 발생
+ */
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+}

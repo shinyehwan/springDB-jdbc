0ed2014885c70898db4224668980691be578ed37
shinyehwan <jghisjl007@naver.com>
2022-07-18 16:17:42 +0900

    트랜잭션 AOP 적용 @Transactional

--- src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -53,15 +53,4 @@ public class MemberServiceV3_1 {
         }
     }
 
-    private void release(Connection con) {
-        if(con != null){
-            try {
-                con.setAutoCommit(true); // 커넥션 풀 고려
-                con.close();
-            } catch (Exception e) {
-                log.info("error", e);
-            }
-        }
-    }
-
 }
